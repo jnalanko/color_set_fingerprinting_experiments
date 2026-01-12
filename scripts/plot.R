430ffdce590ea4012f704c4ed01f9fb5f2623e4b
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-01-13 00:11:02 +0200

    Plot.R

---
+++ scripts/plot.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+df = read.csv("results/table.tsv", sep = "\t", header = TRUE)
+df$tool = as.factor(df$tool)
+df$dataset = as.factor(df$dataset)
+df$mem_bytes = as.numeric(df$mem_bytes)
+df$time_seconds = as.numeric(df$time_seconds)
+
+p = ggplot(df) +
+  geom_line(aes(x = n_genomes, y = mem_bytes, color = tool)) + 
+  geom_line(aes(x = n_genomes, y = mem_bytes, color = tool)) + 
+  facet_wrap(~dataset) +
+  scale_x_log10() + 
+  scale_y_log10() +
+  theme_minimal()
+  #scale_x_continuous(trans = "log2") + 
+  #scale_y_continuous(trans = "log2") +
+  
+
+print(p)
