1f9d53406cd498a6cd3d2cfe07a6f3ea0e833bd9
Alanko Jarno N <analanko@turso03.cs.helsinki.fi>
2026-01-12 23:32:03 +0200

    Print ggcat commands

---
+++ scripts/print_ggcat_commands.py
@@ -0,0 +1,12 @@
+dataset = "random"
+max_power = 14
+
+for i in range(1, max_power+1):
+    n = 2**i
+    input = f"fof/{dataset}_fof_{n}.txt"
+    output = f"ggcat/{dataset}_{n}.fna"
+    log_output = f"logs/{dataset}_{n}_ggcat.log"
+
+    print(f"/usr/bin/time -v ggcat build -c -p -k 31 -m 100 -s 1 -o {output} -j 32 --temp-dir temp --input-lists {input} 2>&1 | tee {log_output}")
+
+# ls unitig-split-1000 | grep fof_part_ | xargs -I {} bash -c "/usr/bin/time -v ggcat build -p -k 31 -m 40 -s 1 -o unitig-split-1000/{}.fna -j 24 --input-lists unitig-split-1000/{} &> logs/{}"
