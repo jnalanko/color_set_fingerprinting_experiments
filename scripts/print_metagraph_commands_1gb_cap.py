ef7b40cdd4958aeca58cc3099490d51d85213c64
Alanko Jarno N <analanko@dx1-523-001.pc.helsinki.fi>
2026-01-21 15:55:51 +0200

    Update metagraph script

--- scripts/print_metagraph_commands_1gb_cap.py
+++ scripts/print_metagraph_commands_1gb_cap.py
@@ -1,18 +1,20 @@
-dataset = "salmonella"
-max_power = 11 # If bigger than this, xargs will split input multiple commands
+binary_path = "../code/bin/metagraph_DNA"
 
-#dataset = "random"
-#max_power = 14
+def print_all(dataset, max_power):
+    for i in range(1, max_power+1):
+        n = 2**i
+        mem_cap_gb = 1
 
-for i in range(1, max_power+1):
-    n = 2**i
-    mem_cap_gb = 1
+        # Build DBG
 
-    # Build DBG
+        input = f"fof/{dataset}_fof_{n}.txt"
+        dbg = f"metagraph/{dataset}_{n}.fna.dbg"
+        anno_output = f"metagraph/{dataset}_{n}_1gb.fna.anno"
+        anno_log_output = f"logs/{dataset}_{n}_metagraph_1gb_anno.log"
+        # Build annotation 
+        print(f"cat {input} | /usr/bin/time -v xargs {binary_path} annotate -i {dbg} -o {anno_output} --anno-filename --verbose --anno-type row -p 32 --fwd-and-reverse --mem-cap-gb {mem_cap_gb} 2>&1 | tee {anno_log_output}")
 
-    input = f"fof/{dataset}_fof_{n}.txt"
-    dbg = f"metagraph/{dataset}_{n}.fna.dbg"
-    anno_output = f"metagraph/{dataset}_{n}_1gb.fna.anno"
-    anno_log_output = f"logs/{dataset}_{n}_metagraph_1gb_anno.log"
-    # Build annotation 
-    print(f"cat {input} | /usr/bin/time -v xargs ~/code/metagraph/metagraph_DNA annotate -i {dbg} -o {anno_output} --anno-filename --verbose --anno-type row -p 32 --fwd-and-reverse --mem-cap-gb {mem_cap_gb} 2>&1 | tee {anno_log_output}")
+# Only up to 11 because xargs will split into multiple commands otherwise
+print_all("random", 11)
+print_all("salmonella", 11)
+
