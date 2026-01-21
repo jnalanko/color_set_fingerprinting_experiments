8c6a51ea7b7d3c83cbecbee8e90faf2827d3e0cc
Alanko Jarno N <analanko@dx1-523-001.pc.helsinki.fi>
2026-01-21 14:45:28 +0200

    Metagraph commands

--- scripts/print_metagraph_commands.py
+++ scripts/print_metagraph_commands.py
@@ -1,20 +1,22 @@
-dataset = "salmonella"
-max_power = 11 # If bigger than this, xargs will split input multiple commands
+binary_path = "../code/bin/metagraph_DNA"
 
-#dataset = "random"
-#max_power = 14
+def print_all(dataset, max_power):
+    for i in range(1, max_power+1):
+        n = 2**i
+        input = f"fof/{dataset}_fof_{n}.txt"
+        output = f"metagraph/{dataset}_{n}.fna"
+        log_output = f"logs/{dataset}_{n}_metagraph.log"
+        mem_cap_gb = 100
 
-for i in range(1, max_power+1):
-    n = 2**i
-    input = f"fof/{dataset}_fof_{n}.txt"
-    output = f"metagraph/{dataset}_{n}.fna"
-    log_output = f"logs/{dataset}_{n}_metagraph.log"
-    mem_cap_gb = 100
+        # Build DBG
+        print(f"cat {input} | /usr/bin/time -v xargs {binary_path} build -k 31 -o {output} -p 32 --mem-cap-gb {mem_cap_gb} --verbose --mode canonical 2>&1 | tee {log_output}")
 
-    # Build DBG
-    print(f"cat {input} | /usr/bin/time -v xargs ~/code/metagraph/metagraph_DNA build -k 31 -o {output} -p 32 --mem-cap-gb {mem_cap_gb} --verbose --mode canonical 2>&1 | tee {log_output}")
+        anno_output = f"metagraph/{dataset}_{n}.fna.anno"
+        anno_log_output = f"logs/{dataset}_{n}_metagraph_anno.log"
+        # Build annotation 
+        print(f"cat {input} | /usr/bin/time -v xargs {binary_path} annotate -i {output}.dbg -o {anno_output} --anno-filename --verbose --anno-type row -p 32 --fwd-and-reverse --mem-cap-gb {mem_cap_gb} 2>&1 | tee {anno_log_output}")
 
-    anno_output = f"metagraph/{dataset}_{n}.fna.anno"
-    anno_log_output = f"logs/{dataset}_{n}_metagraph_anno.log"
-    # Build annotation 
-    print(f"cat {input} | /usr/bin/time -v xargs ~/code/metagraph/metagraph_DNA annotate -i {output}.dbg -o {anno_output} --anno-filename --verbose --anno-type row -p 32 --fwd-and-reverse --mem-cap-gb {mem_cap_gb} 2>&1 | tee {anno_log_output}")
+# Only up to 11 because xargs will split into multiple commands otherwise
+print_all("random", 11)
+print_all("salmonella", 11)
+
