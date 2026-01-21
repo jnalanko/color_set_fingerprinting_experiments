2add1c87801b610fa97deba1b6d4314501294805
Alanko Jarno N <analanko@dx1-523-001.pc.helsinki.fi>
2026-01-21 14:39:08 +0200

    Print bifrost

--- scripts/print_bifrost_commands.py
+++ scripts/print_bifrost_commands.py
@@ -1,14 +1,13 @@
-#dataset = "salmonella"
-#max_power = 17
+def print_all(dataset, max_power):
+    for i in range(1, max_power+1):
+        n = 2**i
+        input = f"fof/{dataset}_fof_{n}.txt"
+        output = f"bifrost/{dataset}_{n}.fna"
+        log_output = f"logs/{dataset}_{n}_bifrost.log"
+        threads = 32
 
-dataset = "random"
-max_power = 14
+        print(f"LD_LIBRARY_PATH=~/code/bifrost_fork/build/lib/ /usr/bin/time -v ~/code/bifrost_fork/build/bin/Bifrost build -r {input} -o {output} -t {threads} --tmp-dir temp -c -v 2>&1 | tee {log_output}")
 
-for i in range(1, max_power+1):
-    n = 2**i
-    input = f"fof/{dataset}_fof_{n}.txt"
-    output = f"bifrost/{dataset}_{n}.fna"
-    log_output = f"logs/{dataset}_{n}_bifrost.log"
-    threads = 16 # Bifrost supports max 16, otherwise refuses to run
+print_all("random", 14)
+print_all("salmonella", 17)
 
-    print(f"LD_LIBRARY_PATH=~/code/bifrost_fork/build/lib/ /usr/bin/time -v ~/code/bifrost_fork/build/bin/Bifrost build -r {input} -o {output} -t {threads} --tmp-dir temp -c -v 2>&1 | tee {log_output}")
