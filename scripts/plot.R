a690a77fe1585ea5c68e11e12b99f8bcc31aa700
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-01-29 12:18:27 +0200

    Plots

--- scripts/plot.R
+++ scripts/plot.R
@@ -7,10 +7,12 @@ df$tool = as.factor(df$tool)
 df$dataset = factor(df$dataset, levels = c("salmonella", "random"), labels = c("Salmonella", "Random"))
 df$mem_bytes = as.numeric(df$mem_bytes)
 df$time_seconds = as.numeric(df$time_seconds)
+df$time_minutes = df$time_seconds / 60
 
 shared_theme =   theme(
   panel.grid.major = element_line(color = "grey70", linewidth = 0.2),
   panel.grid.minor = element_line(color = "grey90", linewidth = 0.2),
+  axis.text.x = element_text(angle = 45, hjust = 1),
 )
 shared_legend = scale_color_manual(
   values = c("bifrost" = "#ffbe0b", "ggcat" = "#fb5607", "metagraph_1gb_anno" = "#ff006e", "themisto" = "#8338ec", "themisto_to_disk" = "#3a86ff"),
@@ -46,8 +48,8 @@ print(p)
 ggsave("plots/mem.pdf")
 
 p = ggplot(df) +
-  geom_line(aes(x = n_genomes, y = time_seconds, color = tool)) + 
-  geom_point(aes(x = n_genomes, y = time_seconds, color = tool)) + 
+  geom_line(aes(x = n_genomes, y = time_minutes, color = tool)) + 
+  geom_point(aes(x = n_genomes, y = time_minutes, color = tool)) + 
   facet_wrap(~dataset) +
   scale_x_log10(
     breaks = 10^(-1:5),
@@ -56,6 +58,11 @@ p = ggplot(df) +
   scale_y_log10(
     breaks = 10^(0:6),
     minor_breaks = rep(1:9, times = 3) * 10^rep(0:6, each = 9)
+  ) +
+  labs(
+    x = "# Genomes",
+    y = "Time (minutes)",
+    color = "Method",
   ) +
   shared_legend + 
   theme_minimal() + shared_theme
