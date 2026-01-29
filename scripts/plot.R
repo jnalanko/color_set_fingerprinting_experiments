72347ff4df65c1cce7fcf9d1cd06758547c29f77
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-01-29 12:29:06 +0200

    Plots

--- scripts/plot.R
+++ scripts/plot.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(cowplot)
 
 df = read.csv("results/table.tsv", sep = "\t", header = TRUE)
 df$tool = as.factor(df$tool)
@@ -13,6 +14,7 @@ shared_theme =   theme(
   panel.grid.major = element_line(color = "grey70", linewidth = 0.2),
   panel.grid.minor = element_line(color = "grey90", linewidth = 0.2),
   axis.text.x = element_text(angle = 45, hjust = 1),
+  plot.title = element_text(hjust = 0.5)
 )
 shared_legend = scale_color_manual(
   values = c("bifrost" = "#ffbe0b", "ggcat" = "#fb5607", "metagraph_1gb_anno" = "#ff006e", "themisto" = "#8338ec", "themisto_to_disk" = "#3a86ff"),
@@ -24,7 +26,7 @@ shared_legend = scale_color_manual(
   name = "Method"
 )
 
-p = ggplot(df) +
+p_mem = ggplot(df) +
   geom_line(aes(x = n_genomes, y = mem_bytes, color = tool)) + 
   geom_point(aes(x = n_genomes, y = mem_bytes, color = tool)) + 
   facet_wrap(~dataset) +
@@ -40,14 +42,15 @@ p = ggplot(df) +
     x = "# Genomes",
     y = "Memory (bytes)",
     color = "Method",
+    title = "Memory"
   ) +
   shared_legend + 
   theme_minimal() + shared_theme
 
-print(p)
+print(p_mem)
 ggsave("plots/mem.pdf")
 
-p = ggplot(df) +
+p_time = ggplot(df) +
   geom_line(aes(x = n_genomes, y = time_minutes, color = tool)) + 
   geom_point(aes(x = n_genomes, y = time_minutes, color = tool)) + 
   facet_wrap(~dataset) +
@@ -63,9 +66,24 @@ p = ggplot(df) +
     x = "# Genomes",
     y = "Time (minutes)",
     color = "Method",
+    title = "Time"
   ) +
   shared_legend + 
   theme_minimal() + shared_theme
 
-print(p)
+print(p_time)
 ggsave("plots/time.pdf")
+
+leg <- get_legend(p_mem + theme(legend.position = "right"))
+p1  <- p_mem  + theme(legend.position = "none")
+p2  <- p_time + theme(legend.position = "none")
+
+p_both = plot_grid(
+  plot_grid(p1, p2, nrow = 1, align = "hv", axis = "tblr"),
+  leg,
+  nrow = 1,
+  rel_widths = c(1, 0.18)
+)
+
+print(p_both)
+ggsave("plots/mem_and_time.pdf", width = 10, height=4)
