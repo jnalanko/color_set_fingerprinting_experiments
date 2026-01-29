6d7de197ab4daf749503bb64c551878dd1a417f5
Alanko Jarno N <analanko@dx1-523-001.pc.helsinki.fi>
2026-01-29 11:59:05 +0200

    d10000

--- scripts/print_themisto2_commands.py
+++ scripts/print_themisto2_commands.py
@@ -1,13 +1,15 @@
+d = 10000
+
 def print_all(dataset, max_power):
     for i in range(1, max_power+1):
         n = 2**i
         input = f"fof/{dataset}_fof_{n}.txt"
         sbwt = f"SBWTs/{dataset}_{n}.sbwt"
         lcs = f"SBWTs/{dataset}_{n}.lcs"
-        output = f"themisto2/{dataset}_{n}.thm2"
-        log_output = f"logs/{dataset}_{n}_themisto.log"
+        output = f"themisto2/{dataset}_{n}_d10000.thm2"
+        log_output = f"logs/{dataset}_{n}_themisto_d10000.log"
 
-        print(f"/usr/bin/time -v themisto2 build -i {input} --sbwt {sbwt} --lcs {lcs} -o {output} --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense 2>&1 | tee {log_output}")
+        print(f"/usr/bin/time -v themisto2 build -i {input} --sbwt {sbwt} --lcs {lcs} -o {output} --temp-dir temp -k 31 -d {d} -t 32 --index-type sparse-dense 2>&1 | tee {log_output}")
 
 print_all("random", 14)
 print_all("salmonella", 17)
