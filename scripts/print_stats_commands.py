6368d18d1f8b2ce923da4fa2a7ac4ef672661d1b
Alanko Jarno N <analanko@dx1-523-001.pc.helsinki.fi>
2026-01-30 11:16:28 +0200

    Stats

--- scripts/print_stats_commands.py
+++ scripts/print_stats_commands.py
@@ -1,10 +1,10 @@
-max_power_salmonella = 15
+max_power_salmonella = 16
 max_power_random = 14
 
 for i in range(1, max_power_salmonella):
     n = 2**i
-    print(f"echo stats for salmonella_{n} && themisto2 stats -t 32 -i themisto2/salmonella_{n}.thm2")
+    print(f"echo stats for salmonella_{n} && themisto2 stats -t 32 -i themisto2/salmonella_{n}_d10000.thm2 > stats/salmonella_{n}_d10000.thm2.stats")
 
 for i in range(1, max_power_random):
     n = 2**i
-    print(f"echo stats for random_{n} && themisto2 stats -t 32 -i themisto2/random_{n}.thm2")
+    print(f"echo stats for random_{n} && themisto2 stats -t 32 -i themisto2/random_{n}_d10000.thm2 > stats/random_{n}_d10000.thm2.stats")
